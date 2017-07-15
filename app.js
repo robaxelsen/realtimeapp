1fafb3dcce1d49f58fab8e9c9289a3b5db112e0f
robeerob <rob@rob.ee>
2017-07-15 08:42:55 +0200

    Introduce React state in our app

--- app.js
+++ app.js
@@ -30,17 +30,33 @@ class ChannelList extends React.Component {
 }
 
 class ChannelForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {};
+  }
   onChange(e) {
-    console.log(e.target.value);
+    this.setState({
+      channelName: e.target.value
+    });
+    // console.log(e.target.value);
   }
   onSubmit(e) {
-
+    let channelName = this.state.channelName;
+    console.log(channelName);
+    channels.push({
+      name: channelName
+    });
+    this.setState({
+      channelName: ''
+    });
+    e.preventDefault();
   }
   render() {
     return(
       <form onSubmit={this.onSubmit.bind(this)}>
         <input type='text'
           onChange={this.onChange.bind(this)}
+          value={this.state.channelName}
         />
       </form>
     )
